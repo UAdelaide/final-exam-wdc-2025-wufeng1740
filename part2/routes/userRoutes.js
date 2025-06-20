bdd5aeaed109302b629e7cda3733908e06309b05
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 12:31:16 +0000

    [automatic commit] 2025-06-20 12:31:16 UTC 202.7.247.25

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,7 +37,7 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  
+  console.log('Login attempt:', req.body);
   const { username, password } = req.body;
 
   try {
