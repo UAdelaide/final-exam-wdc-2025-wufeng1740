b6c586addc0a674d028ef3cb2c6fe6d8c11be111
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 03:47:57 +0000

    [automatic commit] 2025-06-20 03:47:57 UTC 202.7.247.25

--- part1/app.js
+++ part1/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 const db = require(".db.js"); // Import the database module
 
 var indexRouter = require('./routes/index');
