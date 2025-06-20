3c99ad41ce6e34148475bc7d97d8d8fea229129f
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 02:47:24 +0000

    [automatic commit] 2025-06-20 02:47:24 UTC 202.7.247.25

--- part1/test_data.sql
+++ part1/test_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username, avatar, password, date_of_birth, address, email, isAdmin)
+INSERT INTO users (username, email, , date_of_birth, address, email, isAdmin)
 VALUES
   ('admin',    'img/logo_light-mode.png','admin',   '1991-04-14', '123 Main St',        'alice@example.com',  TRUE),
   ('alice',    '/avatars/default.png',   'pass123', '1990-01-01', '123 Main St',        'alice@example.com',  FALSE),
