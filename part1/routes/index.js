b7684171d976969a93ab5a5de4677f2f9ab22c7b
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 04:49:12 +0000

    [automatic commit] 2025-06-20 04:49:12 UTC 202.7.247.25

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('//api/dogs', function(req, res, next) {
+router.get('/api/dogs', function(req, res, next) {
 
 });
 
@@ -14,7 +14,7 @@ router.get('/api/walkrequests/open', function(req, res, next) {
 
 });
 
-router.get('//api/dogs', function(req, res, next) {
+router.get('/api/walkers/summary', function(req, res, next) {
 
 });
 
