2f1d3eda3f0f2e71ad6e79dc01b0e0dbe17c1a89
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 03:40:01 +0000

    [automatic commit] 2025-06-20 03:40:01 UTC 202.7.247.25

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    
+  host: 'localhost',
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
