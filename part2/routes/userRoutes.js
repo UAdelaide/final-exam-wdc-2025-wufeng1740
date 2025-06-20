addeb7409b01be9df045efb444e8dac136133985
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 15:49:40 +0000

    [automatic commit] 2025-06-20 15:49:39 UTC 129.127.202.46

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -32,6 +32,7 @@ router.get('/me', (req, res) => {
   if (!req.session.user) {
     return res.status(401).json({ error: 'Not logged in' });
   }
+  console.log('Fetching current user:', req.session.user);
   res.json(req.session.user);
 });
 
