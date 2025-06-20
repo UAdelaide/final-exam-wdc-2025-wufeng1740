14a5fa03975045d43d9c79c7b3a57651c4b2860e
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 16:22:50 +0000

    [automatic commit] 2025-06-20 16:22:50 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.46

--- part2/routes/indexRoutes.js
+++ part2/routes/indexRoutes.js
@@ -5,7 +5,7 @@ const db = require('../models/db');
 router.get('/api/dogs', async function(req, res, next) {
   try {
     const row = await db.query(`
-      SELECT d.dog_id, d.name as dog_name, d.size, u.username as owner_username
+      SELECT d.dog_id, d.name as dog_name, d.size, d.owner_id, u.username as owner_username
       FROM Dogs d
       JOIN Users u ON d.owner_id = u.user_id
     `);
