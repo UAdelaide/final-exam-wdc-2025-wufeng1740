a4077c39707f73da9f834dadd4df6f0e4e09f25c
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 12:53:01 +0000

    [automatic commit] 2025-06-20 12:53:01 UTC 202.7.247.25

---
+++ part2/db.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'DogWalkService',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+async function query(sql, params) {
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    await connection.beginTransaction();
+    const [rows, fields] = await connection.execute(sql, params);
+    await connection.commit();
+    return rows;
+  } catch (err) {
+    if (connection) {
+      try { await connection.rollback(); } catch (rollbackErr) { /* empty */ }
+    }
+    console.error('❌ SQL Execution Error:', err.message);
+    throw err;
+  } finally {
+    if (connection) connection.release();
+  }
+}
+
+const rawPool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0
+});
+
+async function rawQuery(sql, params) {
+  let connection;
+  try {
+    connection = await rawPool.getConnection();
+    await connection.beginTransaction();
+    const [rows, fields] = await connection.query(sql, params);
+    await connection.commit();
+    return rows;
+  } catch (err) {
+    if (connection) {
+      try { await connection.rollback(); } catch (rollbackErr) { /* empty */ }
+    }
+    console.error('❌ SQL Execution Error:', err.message);
+    throw err;
+  } finally {
+    if (connection) connection.release();
+  }
+}
+
+module.exports = {
+  query,
+  pool,
+  rawQuery,
+  rawPool
+};
