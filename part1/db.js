117d2e42a494f88351dc2789118e713e5f996c92
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 03:40:43 +0000

    [automatic commit] 2025-06-20 03:40:42 UTC 202.7.247.25

--- part1/db.js
+++ part1/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: ,
+  database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
