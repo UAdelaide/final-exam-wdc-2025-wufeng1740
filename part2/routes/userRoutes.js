8111fdbac3adca2b7134f0dd4af550c6a344c1a5
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 11:13:29 +0000

    [automatic commit] 2025-06-20 11:13:29 UTC ;; communications error to 208.67.222.222#53: timed out 202.7.247.25

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -65,7 +65,7 @@ router.post('/login', async (req, res) => {
 // POST logout
 router.post('/logout', (req, res) => {
   if (req.session.user) {
-    req.session.destroy(err => {
+    req.session.destroy((err) => {
       if (err) {
         return res.status(500).json({ error: 'Logout failed' });
       }
