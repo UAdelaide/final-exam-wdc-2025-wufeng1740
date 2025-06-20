b76789b490e1a06931908776d4c32f882c9148d9
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 02:50:09 +0000

    [automatic commit] 2025-06-20 02:50:09 UTC 202.7.247.25

--- part1/test_data.sql
+++ part1/test_data.sql
@@ -3,5 +3,5 @@ VALUES
 ('alice123',    'alice@example.com',   'hashed123', 'owner'),
 ('bobwalker',   'bob@example.com',     'hashed456', 'walker'),
 ('carol123',    'carol@example.com',   'hashed789', 'owner'),
-('david999',    'david@example.com',   'hashed101', 'walker'),
+('davidwalker', 'david@example.com',   'hashed101', 'walker'),
 ('emma2023',    'emma@example.com',    'hashed202', 'owner');
