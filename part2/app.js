399130060c24758341ca1437630446f33f8150bb
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 10:56:39 +0000

    [automatic commit] 2025-06-20 10:56:39 UTC 202.7.247.25

--- part2/app.js
+++ part2/app.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-// Setup for 
+// Setup for session and cookie ------------------------------------------
 // Cookie parser middleware
 app.use(cookieParser());
 // Session middleware
