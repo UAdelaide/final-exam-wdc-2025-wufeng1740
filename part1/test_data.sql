12050d42b01e9fb4a9f69ce3bc52b6cb7032822d
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 02:53:34 +0000

    [automatic commit] 2025-06-20 02:53:34 UTC 202.7.247.25

--- part1/test_data.sql
+++ part1/test_data.sql
@@ -1,11 +1,11 @@
-INSERT INTO users (username, email, password_hash, role) VALUES
+INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123',    'alice@example.com',   'hashed123', 'owner'),
 ('bobwalker',   'bob@example.com',     'hashed456', 'walker'),
 ('carol123',    'carol@example.com',   'hashed789', 'owner'),
 ('davidwalker', 'david@example.com',   'hashed101', 'walker'),
 ('emma123',     'emma@example.com',    'hashed202', 'owner');
 
-INSERT INTO dogs (name, size, owner_id) VALUES
+INSERT INTO Dogs (name, size, owner_id) VALUES
 ('Max', 'medium', (SELECT id FROM users WHERE username = 'alice123')),
 ('Bella', 'small', (SELECT id FROM users WHERE username = 'carol123')),
 ('Rocky', 'large', (SELECT id FROM users WHERE username = 'emma123')),
