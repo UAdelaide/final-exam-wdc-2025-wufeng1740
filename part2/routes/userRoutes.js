d0d2f12442a311750e867ffa3f0c6f10fa094593
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 11:21:15 +0000

    [automatic commit] 2025-06-20 11:21:15 UTC 202.7.247.25

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -42,8 +42,8 @@ router.post('/login', async (req, res) => {
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE email = ? AND password_hash = ?
-    `, [email, password]);
+      WHERE username = ? AND password_hash = ?
+    `, [username, password]);
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
