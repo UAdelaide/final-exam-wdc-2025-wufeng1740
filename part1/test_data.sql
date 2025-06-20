98e940437c594371f7d0a45067cd97a738cbbb4a
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 02:51:52 +0000

    [automatic commit] 2025-06-20 02:51:52 UTC 202.7.247.25

--- part1/test_data.sql
+++ part1/test_data.sql
@@ -1,5 +1,4 @@
-INSERT INTO users (username, email, password_hash, role)
-VALUES
+INSERT INTO users (username, email, password_hash, role) VALUES
 ('alice123',    'alice@example.com',   'hashed123', 'owner'),
 ('bobwalker',   'bob@example.com',     'hashed456', 'walker'),
 ('carol123',    'carol@example.com',   'hashed789', 'owner'),
