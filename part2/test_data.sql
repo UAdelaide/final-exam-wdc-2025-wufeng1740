3926f688894675f7bd0ea997f6264505b1217568
Feng Wu <feng.wu@student.adelaide.edu.au>
2025-06-20 15:12:52 +0000

    [automatic commit] 2025-06-20 15:12:51 UTC 129.127.202.46

--- part2/test_data.sql
+++ part2/test_data.sql
@@ -22,7 +22,7 @@ INSERT INTO Dogs (name, size, owner_id) VALUES
 ('Lucy', 'small', (SELECT user_id FROM Users WHERE username = 'ownerJane')),
 ('Rocky', 'large', (SELECT user_id FROM Users WHERE username = 'ownerBob')),
 ('Daisy', 'medium', (SELECT user_id FROM Users WHERE username = 'ownerBob')),
-('Luna', 'small', (SELECT user_id FROM Users WHERE username = 'ownerBob'));
+('Milo', 'small', (SELECT user_id FROM Users WHERE username = 'ownerBob'));
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
